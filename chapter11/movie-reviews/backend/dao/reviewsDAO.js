89b7c13f7bc0072b8908e13d0f224683ca419bd5
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-09-14 19:14:13 +0200

    fourth commit

--- chapter11/movie-reviews/backend/dao/reviewsDAO.js
+++ chapter11/movie-reviews/backend/dao/reviewsDAO.js
@@ -1,4 +1,4 @@
-import mongodb from "mongodb";
+import mongodb from 'mongodb';
 
 const ObjectId = mongodb.ObjectId;
 let reviews;
@@ -6,39 +6,39 @@ let reviews;
 export default class ReviewsDAO {
     static async injectDB(conn) {//injectDB function
         if (reviews) {
-            return
+            return;
         }
         try {
-            reviews = await
-                conn.db(process.env.MOVIEREVIEWS_NS).collection('reviews')//access database reviews
+            reviews = await conn.db(process.env.MOVIEREVIEWS_NS).collection('reviews')//access database reviews
+            console.log('Connected to the database successfully.'); // Log a successful connection
         }
         catch (e) {
-            console.error(`unable to establish connection handle in
-reviewDAO: ${e}`)
+            console.error(`unable to establish connection handle in reviewDAO: ${e}`)
         }
     }
 
-    static async addReview(movieId, user, review, date) {//creating a review
+    static async addReview(movieId, user, review) {//creating a review
         try {
+            const date = new Date();
             const reviewDoc = {//create a reviewDoc document object
                 name: user.name,
                 user_id: user._id,
                 date: date,
                 review: review,
-                movie_id: ObjectId(movieId)//convert movieId string to a MongoDB object id
-            }
+                movie_id: new ObjectId(movieId)//convert movieId string to a MongoDB object id
+            };
             return await reviews.insertOne(reviewDoc)
         }
         catch (e) {
             console.error(`unable to post review: ${e}`)
-            return { error: e }
+            return { error: e };
         }
     }
 
     static async updateReview(reviewId, userId, review, date) {//editing a review
         try {
             const updateResponse = await reviews.updateOne(
-                { user_id: userId, _id: ObjectId(reviewId) },//filter for an existing review created by userId and with reviewId
+                { user_id: userId, _id: new ObjectId(reviewId) },//filter for an existing review created by userId and with reviewId
                 { $set: { review: review, date: date } }//if review exits: filter with new review text and date
             )
             return updateResponse
@@ -51,10 +51,12 @@ reviewDAO: ${e}`)
 
     static async deleteReview(reviewId, userId) {//deleting a review:
         try {
+            console.log(`Deleting review with review_id: ${reviewId} and user_id: ${userId}`);
             const deleteResponse = await reviews.deleteOne({
-                _id: ObjectId(reviewId),//specify ObjectId(reviewId)
-                user_id: userId, //to chekc reviewId and created by userId 
-            })
+                _id: new ObjectId(reviewId),//specify ObjectId(reviewId)
+                user_id: userId //to chekc reviewId and created by userId 
+            });
+            console.log('Review deletion response:', deleteResponse);
             return deleteResponse//and delete it if it exists
         }
         catch (e) {
@@ -62,5 +64,4 @@ reviewDAO: ${e}`)
             return { error: e }
         }
     }
-
 }
