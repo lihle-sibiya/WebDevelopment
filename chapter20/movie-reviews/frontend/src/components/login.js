2555ef91f7f02d1870915b3b2394c0a88c7cb3e1
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-09-18 23:51:54 +0200

    second commit

--- chapter20/movie-reviews/frontend/src/components/login.js
+++ chapter20/movie-reviews/frontend/src/components/login.js
@@ -1,22 +1,26 @@
-import React, { useState } from 'react'
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+import {Form, Button} from 'react-bootstrap';
+
 
 const Login = props => {
-    const [name, setName] = useState("")
-    const [id, setId] = useState("")
-    const onChangeName = e => {
-        const name = e.target.value
+    const [name, setName] = useState("");
+    const [id, setId] = useState("");
+
+    const onChangeName = e => {//binds the field value to the name state variables
+        const name = e.target.value;
         setName(name);
-    }
-    const onChangeId = e => {
-        const id = e.target.value
+    };
+
+    const onChangeId = e => {///binds the field value to the id state variables
+        const id = e.target.value;
         setId(id);
-    }
-    const login = () => {
-        props.login({ name: name, id: id })
-        props.history.push('/')
-    }
+    };
+
+    const login = () => {//called by Submit button
+        props.login({ name: name, id: id });//passed by login route on App.js
+        props.history.push('/');//redirected after loggin in
+    };
+
     return (
         <div>
             <Form>
@@ -43,7 +47,7 @@ const Login = props => {
                 </Button>
             </Form>
         </div>
-    )
-}
+    );
+};
 
 export default Login;
