9aadad22fc76925efbff7341488bbcad2e4a6438
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-09-21 21:30:30 +0200

    second commit

--- chapter22/movie-reviews/backend/api/movies.controller.js
+++ chapter22/movie-reviews/backend/api/movies.controller.js
@@ -1,54 +1,66 @@
-import MoviesDAO from '../dao/moviesDAO.js';
+import MoviesDAO from '../dao/moviesDAO.js' 
 
-export default class MoviesController {
-    static async apiGetMovies(req, res, next) {//query string with key-value pairs: eg:title:"dragon"
-        const moviesPerPage = req.query.moviesPerPage ? parseInt(req.query.moviesPerPage) : 20; //if moviesPerPage exists,parse into integer
-        const page = req.query.page ? parseInt(req.query.page) : 0; //parse into integer
+export default class MoviesController{
 
-        let filters = {}; //no filters are applied at first
-        if (req.query.rated) {//check if the rated query string exists
-            filters.rated = req.query.rated;//add to the filters object
+    static async apiGetMovies(req,res,next){
+        //displays whats typed in or 20 movies/page
+        const moviesPerPage = req.query.moviesPerPage ? parseInt(req.query.moviesPerPage) : 20
+        //displays whats typed in or page 0
+        const page = req.query.page ? parseInt(req.query.page) : 0
+
+        //whats typed is pushed to filters object, updating values
+        let filters = {} 
+        if(req.query.rated){            
+            filters.rated = req.query.rated
+        } 
+        else if(req.query.title){            
+            filters.title = req.query.title            
         }
-        else if (req.query.title) {//check if the title query string exists
-            filters.title = req.query.title;//add to the filters object
-        }
-
-        const { moviesList, totalNumMovies } = await MoviesDAO.getMovies({ filters, page, moviesPerPage })//call getMovies in MoviesDAO
-        let response = {//jason response object to a user whoc calls the URL
+         
+        //links to movies DAO to access filters
+        const { moviesList, totalNumMovies } = await MoviesDAO.getMovies({filters, page, moviesPerPage})
+        
+        //response displays in json format
+        let response ={
             movies: moviesList,
             page: page,
             filters: filters,
             entries_per_page: moviesPerPage,
             total_results: totalNumMovies,
-        };
-        res.json(response);
+        }
+        res.json(response) 
     }
-
-    static async apiGetMovieById(req, res, next) {//apiGetMovieById
-        try {
-            let id = req.params.id || {};//for an id parameter in URL eg: localhost:5000/api/v1/movies/id/12345
-            let movie = await MoviesDAO.getMovieById(id);//return specific movie in JSON
-            if (!movie) {//if no movie
-                res.status(404).json({ error: "not found" })//return erro
-                return;
+    static async apiGetMovieById(req,res, next){
+        try{
+            //look for parameters, whats typed atthe end of the url
+            let id = req.params.id || {}
+            //id is the identifier
+            let movie = await MoviesDAO.getMovieById(id)
+            //error handling if movie id isn't found in movie object
+            if(!movie){ 
+                res.status(404).json({ error: "not found"})
+                return
             }
-            res.json(movie);
+            res.json(movie)
         }
-        catch (e) {
-            console.log(`api, ${e}`);
-            res.status(500).json({ error: e });
+        catch(e){
+            //error message
+            console.log(`api, ${e}`)
+            res.status(500).json({error: e})
         }
     }
 
-
-    static async apiGetRatings(req, res, next) {//apiGetRatings
-        try {
-            let propertyTypes = await MoviesDAO.getRatings();//call MoviesDAO
+    static async apiGetRatings(req,res,next){
+        try{ 
+            //respond with property in json format
+            let propertyTypes = await MoviesDAO.getRatings()
             res.json(propertyTypes)
         }
-        catch (e) {
-            console.log(`api,${e}`);
-            res.status(500).json({ error: e });
+        catch(e){
+            //error message
+            console.log(`api,${e}`)
+            res.status(500).json({error: e})
         }
     }
+
 }
--- chapter22/movie-reviews/backend/api/movies.route.js
+++ chapter22/movie-reviews/backend/api/movies.route.js
@@ -1,20 +1,23 @@
-import express from 'express';
-import MoviesController from './movies.controller.js';
-import ReviewsController from './reviews.controller.js';
+import express from 'express'
+import MoviesController from './movies.controller.js'
+import ReviewsController from './reviews.controller.js' 
 
+// get access to express router
+const router = express.Router() 
 
-const router = express.Router(); 
+//router.route('/').get((req,res) => res.send('hello world'))
+//gets full movie object
+router.route('/').get(MoviesController.apiGetMovies)
 
-router.route('/').get(MoviesController.apiGetMovies);
-router.route('/id/:id').get(MoviesController.apiGetMovieById)
-router.route('/ratings').get(MoviesController.apiGetRatings)
+//route gets specific movies and their ratings
+router.route("/id/:id").get(MoviesController.apiGetMovieById)
+router.route("/ratings").get(MoviesController.apiGetRatings)
 
-
+//CRUD functioning for reviews
 router
-    .route('/review')
+    .route("/review")
     .post(ReviewsController.apiPostReview)
     .put(ReviewsController.apiUpdateReview)
     .delete(ReviewsController.apiDeleteReview)
-
 
 export default router
