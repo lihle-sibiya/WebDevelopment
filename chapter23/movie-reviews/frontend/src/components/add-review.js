902e73952720d7afadec36270e3e7290cf7fb12e
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-09-21 22:14:40 +0200

    first commit

--- chapter23/movie-reviews/frontend/src/components/add-review.js
+++ chapter23/movie-reviews/frontend/src/components/add-review.js
@@ -4,83 +4,88 @@ import { Link } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 
 
-
 const AddReview = props => {
-    let editing = false;//we are in adding mode. True if editing mode
-    let initialReviewState = "";
 
-    //check if a state is passed into AddReview and contains a currentReview property
-    if (props.location.state && props.location.state.currentReview) {
-        editing = true;//if so: editing true
-        initialReviewState = props.location.state.currentReview.review;//and initialReviewState to currentReview’s review
-    }
+   let editing = false
+   let initialReviewState = ''
 
-    const [review, setReview] = useState(initialReviewState);
-    // keeps track if review is submitted
-    const [submitted, setSubmitted] = useState(false);
+   //checks state to allow for editting to occur
+   if (props.location.state && props.location.state.currentReview) {
+      editing = true
+      initialReviewState = props.location.state.currentReview.review
+   }
 
-    const onChangeReview = e => {
-        const review = e.target.value;//keeps track - review value by user
-        setReview(review);
-    };
+   const [review, setReview] = useState(initialReviewState)
+   //keeps track if review's submitted
+   const [submitted, setSubmitted] = useState(false)
 
-    const saveReview = () => {
-        var data = {
-            review: review,
-            name: props.user.name,
-            user_id: props.user.id, movie_id: props.match.params.id // get movie id direct from url
-        };
+   const onChangeReview = e => {
+      const review = e.target.value
+      setReview(review);
+   }
 
-        if (editing) {//if editing is true
-            //we get existing review id
-            data.review_id = props.location.state.currentReview._id
-            MovieDataService.updateReview(data)//and cll updateReview in MovieDataService
-                .then(response => {
-                    setSubmitted(true);
-                    console.log(response.data);
-                })
-                .catch(e => {
-                    console.log(e);
-                });
-        }
-        else {
-            MovieDataService.createReview(data)
-                .then(response => {
-                    setSubmitted(true);
-                })
-                .catch(e => {
-                    console.log(e);
-                });
-        };
-    };
+   const saveReview = () => {
+      var data = {
+         review: review,
+         name: props.user.name,
+         user_id: props.user.id,
+         movie_id: props.match.params.id // get movie id direct from url
+      }
 
-        return (
+      if (editing) {
+         // get existing review id
+         data.review_id = props.location.state.currentReview._id
+         MovieDataService.updateReview(data)
+            .then(response => {
+               setSubmitted(true);
+               console.log(response.data)
+            })
+            .catch(e => {
+               console.log(e);
+            })
+      }
+      else {
+         //create a new review
+         MovieDataService.createReview(data)
+            .then(response => {
+               setSubmitted(true)
+               console.log(response.data)
+            })
+            .catch(e => {
+               console.log(e);
+            })
+      }
+   }
+
+
+   return (
+      <div>
+         {/* uses a ternary operator */}
+         {submitted ? (
             <div>
-                {submitted ? (
-                    <div>
-                        <h4>Review submitted successfully</h4>
-                        <Link to={"/movies/" + props.match.params.id}>
-                            Back to Movie
-                        </Link>
-                    </div>
-                ) : (
-                    <Form>
-                        <Form.Group>
-                            <Form.Label>{editing ? "Edit" : "Create"} Review</Form.Label>
-                            <Form.Control
-                                type="text"
-                                required
-                                value={review}
-                                onChange={onChangeReview}
-                            />
-                        </Form.Group>
-                        <Button variant="primary" onClick={saveReview}>
-                            Submit
-                        </Button>
-                    </Form>
-                )}
+               <h4>Review submitted successfully</h4>
+               <Link to={'/movies' + props.match.params.id}>
+                  Back to Movie
+               </Link>
             </div>
-        );
-    };
+         ) : (
+            <Form>
+               <Form.Group>
+                  <Form.Label>{editing ? "Edit" : "Create"} Review</Form.Label>
+                  <Form.Control
+                     type="text"
+                     required
+                     value={review}
+                     onChange={onChangeReview}
+                  />
+               </Form.Group>
+               <Button variant="primary" onClick={saveReview}>
+                  Submit
+               </Button>
+            </Form>
+         )}
+      </div>
+   )
+}
 
-    export default AddReview;
+export default AddReview;
